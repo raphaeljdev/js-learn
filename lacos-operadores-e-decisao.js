401508e95c4a54ca307b57131ad74eaf616a650b
raphael <raphaelj.dev@gmail.com>
2026-01-05 18:20:27 -0300

    Realiza ate tarefa numero 4

---
+++ lacos-operadores-e-decisao.js
@@ -0,0 +1,55 @@
+// 1. Contador de 1 a 10
+// Use um for para mostrar no console os números de 1 até 10, um por linha.
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log('\n');
+
+// 2. Soma de 1 a 100
+// Some todos os números de 1 a 100. Mostre o resultado no final com uma mensagem explicativa.
+
+let soma = 0;
+
+for (let i = 1; i <= 100; i++) {
+  soma = soma + i;
+}
+
+console.log(`soma de 1 a 100: ${soma}\n`);
+
+// 3. Tabuada personalizada
+// Peça um número e mostre a tabuada desse número (de 1 a 10) usando um for.
+
+let numeroEscolhido = 7;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(numeroEscolhido * i);
+}
+console.log('\n');
+
+// 4. Contagem regressiva
+// Use while para contar de 10 até 0 no console. Mostre uma mensagem no fim: "Contagem finalizada!".
+let contador = 10;
+while (contador >= 0) {
+  console.log(contador);
+  contador--;
+}
+console.log('contagem finalizada\n');
+
+// 5. Receber números até digitar 0
+// Use do...while para simular a entrada de números. O programa deve continuar até que o usuário digite 0. Ao final, mostre quantos números foram digitados.
+
+// 6. Jogo do número secreto
+// Defina uma variável numeroSecreto com o valor 7. O usuário tem 3 tentativas para acertar (simule essas tentativas com variáveis). Mostre mensagens de "Acertou!" ou "Tente novamente".
+
+// 7. Idade ao longo dos anos
+// Crie uma variável anoNascimento e anoAtual. Use um for para listar a idade da pessoa ano a ano até o ano atual.
+
+// 8. Listando números pares
+// Mostre todos os números pares entre 1 e 50 usando for.
+
+// 9. Contar múltiplos de 3 entre 1 e 100
+// Mostre no console quantos números entre 1 e 100 são divisíveis por 3.
+
+// 10. Menu com repetição
+// Crie um menu simples que exibe opções como "1 - Ver saldo", "2 - Fazer depósito", "3 - Sair". Use do...while para repetir o menu até o usuário escolher sair.
