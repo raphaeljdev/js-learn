1254450b2db47bfed3d97a2e87587a95c2d3c8a3
raphael <raphaelj.dev@gmail.com>
2026-01-06 11:12:46 -0300

    Estrutura inicial de funcoes

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function saudacao(name) {
+  console.log(`Hello ${name}`);
+}
+
+function calcularDobro(x) {
+  return x ** 2;
+}
+
+const numeroDobrado = calcularDobro(4);
+
+console.log(numeroDobrado);
