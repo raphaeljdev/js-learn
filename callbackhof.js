5a8238ad902a7d7dfaa9af62693d9b99953be86b
raphael <raphaelj.dev@gmail.com>
2026-01-06 11:56:52 -0300

    finaliza essao de callback

--- callbackhof.js
+++ callbackhof.js
@@ -12,11 +12,15 @@ const divisao = (numero_um, numero_dois) => {
   return numero_um / numero_dois;
 };
 
-const multiplicacao = (numero_um, numero_2) => numero_um * numero_2;
+const multiplicacao = (numero_um, numero_dois) => numero_um * numero_dois;
+
+const subtracao = (numero_um, numero_dois) => numero_um - numero_dois;
 
 const numeroSomado = calcular(5, 5, soma);
 const numeroDividido = calcular(10, 2, divisao);
 const numeroMultiplicado = calcular(10, 2, multiplicacao);
+const numeroSubtraido = calcular(10, 2, subtracao);
 console.log(numeroSomado);
 console.log(numeroDividido);
 console.log(numeroMultiplicado);
+console.log(numeroSubtraido);
